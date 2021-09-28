8ab19467949dd628e1562f6784dae54f039bf0c0
HongBeen <redbean-2@naver.com>
2021-09-29 01:35:12 +0900

    007 프로젝트 성능개선

--- APIs/007/007.js
+++ APIs/007/007.js
@@ -3,17 +3,22 @@ const target = document.querySelector(".target");
 const lineHorizontal = document.querySelector(".line.horizontal");
 const lineVertical = document.querySelector(".line.vertical");
 
-document.addEventListener("mousemove", showCoor);
-document.addEventListener("mouseenter", showCoor);
-function showCoor(event) {
-	const x = event.pageX;
-	const y = event.pageY;
-
-	coord.innerText = `${x}px, ${y}px`;
-	coord.style.top = `${y}px`;
-	coord.style.left = `${x}px`;
-	target.style.top = `${y}px`;
-	target.style.left = `${x}px`;
-	lineHorizontal.style.top = `${y}px`;
-	lineVertical.style.left = `${x}px`;
-}
+addEventListener('load',()=>{
+	const targetRect=target.getBoundingClientRect();
+	const targetHalfWidth=targetRect.width/2;
+	const targetHalfHeight=targetRect.height/2;
+	
+	document.addEventListener("mousemove", showCoor);
+	document.addEventListener("mouseenter", showCoor);
+	function showCoor(event) {
+		const x = event.pageX;
+		const y = event.pageY;
+	
+		coord.innerText = `${x}px, ${y}px`;
+		coord.style.transform = `translate(${x}px,${y}px)`;
+		target.style.transform = `translate(${x-targetHalfWidth}px,${y-targetHalfHeight}px)`;
+		lineHorizontal.style.transform = `translateY(${y}px)`;
+		lineVertical.style.transform = `translateX(${x}px)`;
+	}
+	
+})
