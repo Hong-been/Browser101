b36a8422f80e463f2062e037d6fe424eb4e68773
HongBeen <redbean-2@naver.com>
2021-10-01 04:19:12 +0900

    쇼핑리스트 페이지 클래스네임,스타일링, 자바스크립트 동작 개선
    - 마크업 태그네임 간결하게 수정.
    - 배경컬러 그래디언트추가, 버튼컬로도 색상에 맞춰 조정.
    - 버튼에 마우스올리면 사이즈 증가, 컬러변화, 회전효과 추가.
    - 아무데서나 엔터, 추가버튼 클릭, 삭제버튼 클릭시 input에 포커스 설정.
    - main.js 함수 쪼개서 세분화.

--- DOM/shopping-list/main.js
+++ DOM/shopping-list/main.js
@@ -1,61 +1,68 @@
-const counter=document.querySelector(".title__counter");
-const inputBox=document.querySelector(".bottom__input");
-const inputBtn=document.querySelector(".bottom__add-btn");
-const listItems=document.querySelector(".list__items");
-const emptyMesaage=document.querySelector(".block__empty");
+const counter = document.querySelector(".top__counter");
+const inputBox = document.querySelector(".bottom__input");
+const inputBtn = document.querySelector(".bottom__add");
+const listItems = document.querySelector(".list__items");
+const emptyMesaage = document.querySelector(".list__empty");
 
-inputBtn.addEventListener('click',(event)=>{
-  event.preventDefault();
-  const inputText=inputBox.value;
-  if(inputText){
-    emptyMesaage.style.opacity=0;
-    const li=document.createElement("li");
-    const div=document.createElement("div");
-    const button=document.createElement("button");
+addEventListener("keypress",()=>inputBox.focus());
 
-    li.setAttribute('class','item');
-    div.setAttribute('class','item__content');
-    div.innerText=inputText;
-    button.setAttribute('class','item__delete-btn');
-    button.innerText='﹣';
-    button.addEventListener("click",deleteItem);
+inputBtn.addEventListener("click", (event) => {
+	event.preventDefault();
+	const inputText = inputBox.value;
+	if (!inputText) {
+		inputBox.focus();
+		return;
+	}
+	const row = createRow(inputText);
 
-    li.appendChild(div);
-    li.appendChild(button);
-    listItems.appendChild(li);
+	listItems.appendChild(row);
+	animateOpacity(row, 0, 1);
 
-    const showSmoothKeyframes=new KeyframeEffect(li,
-      [
-        { opacity:0 }, 
-        { opacity:1 }
-    ], { duration:80 });
-    const showSmooth=new Animation(showSmoothKeyframes, document.timeline);
-    
-    showSmooth.play();
-    setTimeout(()=>{
-      counter.innerText=listItems.childElementCount; },250);
+	inputBox.value = "";
+	inputBox.focus();
+	counter.innerText = listItems.childElementCount;
+});
+function createRow(inputText) {
+	emptyMesaage.style.opacity = 0;
+	const li = document.createElement("li");
+	const div = document.createElement("div");
+	const button = document.createElement("button");
 
-    inputBox.value='';
-    counter.innerText=listItems.childElementCount;
-  }
-})
+	li.setAttribute("class", "item__row");
+	div.setAttribute("class", "item__content");
+	div.innerText = inputText;
+	button.setAttribute("class", "item__delete");
+	button.innerText = "﹣";
+	button.addEventListener("click", deleteItem);
 
-function deleteItem(event){
-  const parent=event.target.parentNode;
-  const showSmoothKeyframes=new KeyframeEffect(parent,
-    [
-      { opacity:1 }, 
-      { opacity:0 }
-  ], { duration: 300, endDelay:100 });
-  const moveSmooth=new Animation(showSmoothKeyframes, document.timeline);
-  
-  moveSmooth.play();
-  setTimeout(()=>{
-    parent.remove();
-    counter.innerText=listItems.childElementCount;
-    if(counter.innerText==0){
-      emptyMesaage.style.opacity=1;
-    } },200);
-  
-  
+	li.appendChild(div);
+	li.appendChild(button);
+
+	return li;
+}
+
+async function deleteItem(event) {
+	const parent = event.target.parentNode;
+	await animateOpacity(parent, 1, 0);
+	parent.remove();
+
+	counter.innerText = listItems.childElementCount;
+	if (counter.innerText == 0) {
+		emptyMesaage.style.opacity = 1;
+	}
+  inputBox.focus();
+}
+function animateOpacity(element, from, to) {
+	const animateOpacityKeyframes = new KeyframeEffect(
+		element,
+		[{ opacity: from }, { opacity: to }],
+		{ duration: 300 }
+	);
+	const animateOpacity = new Animation(
+		animateOpacityKeyframes,
+		document.timeline
+	);
+
+	animateOpacity.play();
+	return animateOpacity.finished;
 }
