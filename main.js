f13d404e4ef1511bc91bcabdffd1e487bb046c22
HongBeen <redbean-2@naver.com>
2021-10-06 19:37:39 +0900

    코드정리: 반복되는 코드 변수화, 함수화, 게임중이 아닐때 요소숨기기

--- main.js
+++ main.js
@@ -1,42 +1,55 @@
+'use strict';
+
+const CARROT_SIZE=80;
+const WINNER="🤩 WOW! YOU WIN! 🤩";
+const ITEM_COUNT=10;
+const DURATION_SEC=5;
+
+const introWindow=document.querySelector(".intro");
+const playBtn=document.querySelector(".intro__play");
 const timer=document.querySelector(".header__timer");
-const result=document.querySelector(".result");
+const result=document.querySelector(".field__result");
 const replayBtn=document.querySelector(".result__replay");
 const resultMessage=document.querySelector(".result__message");
 const counter=document.querySelector(".header__remaining");
 const pauseBtn=document.querySelector(".pause");
-const stage=document.querySelector(".items");
-const mins=0, secs=10, duration=mins*60+secs;
-const winMessage="🤩 WOW! YOU WIN! 🤩";
+const field=document.querySelector(".field__items");
 let isPaused=false;
+
 const bgSound=new Audio("./sound/bg.mp3");
 const bugSound=new Audio("./sound/bug_pull.mp3");
 const carrotSound=new Audio("./sound/carrot_pull.mp3");
 const winSound=new Audio("./sound/game_win.mp3");
 const alertSound=new Audio("./sound/alert.wav");
 
-window.addEventListener('load',startGame);
-replayBtn.addEventListener('click',()=>window.location.reload());
-pauseBtn.addEventListener('click',()=>{
+window.addEventListener('load',()=>{
+  counter.innerText=ITEM_COUNT;
+  addItem('carrot',ITEM_COUNT,'img/carrot.png');
+  addItem('bug',ITEM_COUNT,'img/bug.png');
+  playBtn.addEventListener('click',startGame);
+});
+
+function controlPause(){
+  timer.classList.toggle("hidden");
+  field.classList.toggle("hidden");
   if(!isPaused){
     timerObj.pause();
-    stage.removeEventListener('click',removeItem);
+    field.removeEventListener('click',removeItem);
     pauseBtn.innerHTML=`<i class="fas fa-play"></i>`;
     bgSound.pause();
     playAudio(alertSound);
   }
   else{
     timerObj.resume();
-    stage.addEventListener('click',removeItem);
+    field.addEventListener('click',removeItem);
     pauseBtn.innerHTML=`<i class="fas fa-pause"></i>`;
     playAudio(bgSound);
   }
   isPaused=!isPaused;
-});
-stage.addEventListener('click',removeItem);
-
+}
 function removeItem(event){
   const {target}=event;
-  if(!target.classList.contains("item")) return;
+  if(target.tagName!="IMG") return;
   if(target.classList.contains("bug")) {
     playAudio(bugSound);
     return;
@@ -45,9 +58,8 @@ function removeItem(event){
   playAudio(carrotSound);
   counter.innerText-=1;
   if(counter.innerText==0){
-    resultMessage.innerText=winMessage;
-    endGame();
-    playAudio(winSound);
+    resultMessage.innerText=WINNER;
+    endGame(true);
   }
 }
 function playAudio(audio){
@@ -56,57 +68,57 @@ function playAudio(audio){
 }
 
 function startGame(){
-  playAudio(bgSound);
-  result.style.zIndex="-1";
-  result.style.opacity=0;
-  counter.innerText=10;
-  pauseBtn.innerHTML=`<i class="fas fa-pause"></i>`;
-  timerObj.start();
-  showRandomItems();
+  replayBtn.addEventListener('click',()=>window.location.reload());
+  pauseBtn.addEventListener('click',controlPause);
+  field.addEventListener('click',removeItem);
 
-  const items=document.querySelectorAll(".item");
-  items.forEach(item=>{
+  pauseBtn.classList.remove("hidden");
+  counter.classList.remove("hidden");
+  timer.classList.remove("hidden");
+  field.classList.remove("hidden");
+  
+  introWindow.classList.toggle("hidden");
+  playAudio(bgSound);
+  timerObj.start();
+  
+  field.childNodes.forEach(item=>{
+    if(item.tagName!='IMG') return;
     item.addEventListener('mouseenter',()=>{
-    item.style.transform+="scale(1.2)";})
+      item.classList.toggle("bigger");
+    console.log(item);})
     item.addEventListener('mouseleave',()=>{
-      item.style.transform+="scale(0.8)";})
+      item.classList.toggle("bigger");})
   }
   );
 }
-function endGame(){
-  stage.removeEventListener('click',removeItem);
+function endGame(isWinning){
+  field.removeEventListener('click',removeItem);
+  pauseBtn.removeEventListener('click',controlPause);
+  playBtn.removeEventListener('click',startGame);
+
   bgSound.pause();
   timerObj.pause();
-  result.style.zIndex="1";
-  result.style.opacity=1;
+  result.classList.toggle("hidden");
+  if(isWinning) playAudio(winSound);
+  else playAudio(alertSound);
 }
-function showRandomItems(){
-  const {width,height}=stage.getBoundingClientRect();
+function addItem(className,num,imgPath){
+  const width=field.getBoundingClientRect().width-CARROT_SIZE;
+  const height=field.getBoundingClientRect().height-CARROT_SIZE;
   const fragment = new DocumentFragment();
 
-  for(let i=0;i<counter.innerText;i++){
+  for(let i=0;i<num;i++){
     const random=(max)=>Math.floor(Math.random() * max);
     const top=random(height);
     const left=random(width);
     
     const item=document.createElement('img');
-    item.setAttribute('src','img/carrot.png');
-    item.classList.add('item','carrot');
-    item.style.transform=`translate(${left}px,${top}px)`;
+    item.setAttribute('src',imgPath);
+    item.setAttribute('class',className);
+    item.style.transform=`translate(${left}px,${top}px) scale(1.0)`;
     fragment.appendChild(item);
   }
-  for(let i=0;i<counter.innerText;i++){
-    const random=(max)=>Math.floor(Math.random() * max);
-    const top=random(height);
-    const left=random(width);
-
-    const item=document.createElement('img');
-    item.setAttribute('src','img/bug.png');
-    item.classList.add('item','bug');
-    item.style.transform=`translate(${left}px,${top}px)`;
-    fragment.appendChild(item);
-  }
-  stage.appendChild(fragment);
+  field.appendChild(fragment);
 }
 class Timer{
   constructor(duration,display,printDone){
@@ -131,7 +143,7 @@ class Timer{
     if(this.duration<=0){
       clearInterval(this.interval);
       this.interval=null;
-      this.callBack("Done");
+      this.callBack(false);
     }
     this.mins=`${parseInt(this.duration/60)}`;
     this.secs=`${this.duration%60}`;
@@ -145,4 +157,4 @@ class Timer{
     }
   }
 }
-const timerObj=new Timer(duration,timer,endGame);
+const timerObj=new Timer(DURATION_SEC,timer,endGame);
