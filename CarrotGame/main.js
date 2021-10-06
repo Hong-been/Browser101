6e9b4b852612bed4fe681b7ab6df07762acfcbd5
HongBeen <redbean-2@naver.com>
2021-10-06 17:18:50 +0900

    인트로 팝업창에 시작버튼 추가

--- CarrotGame/main.js
+++ CarrotGame/main.js
@@ -1,11 +1,13 @@
+const introWindow=document.querySelector(".intro");
+const playBtn=document.querySelector(".intro__play");
 const timer=document.querySelector(".header__timer");
-const result=document.querySelector(".result");
+const result=document.querySelector(".field__result");
 const replayBtn=document.querySelector(".result__replay");
 const resultMessage=document.querySelector(".result__message");
 const counter=document.querySelector(".header__remaining");
 const pauseBtn=document.querySelector(".pause");
-const stage=document.querySelector(".items");
-const mins=0, secs=10, duration=mins*60+secs;
+const field=document.querySelector(".field__items");
+const mins=0, secs=5, duration=mins*60+secs;
 const winMessage="🤩 WOW! YOU WIN! 🤩";
 let isPaused=false;
 const bgSound=new Audio("./sound/bg.mp3");
@@ -14,26 +16,29 @@ const carrotSound=new Audio("./sound/carrot_pull.mp3");
 const winSound=new Audio("./sound/game_win.mp3");
 const alertSound=new Audio("./sound/alert.wav");
 
-window.addEventListener('load',startGame);
-replayBtn.addEventListener('click',()=>window.location.reload());
-pauseBtn.addEventListener('click',()=>{
+window.addEventListener('load',()=>{
+  playBtn.addEventListener('click',startGame);
+  replayBtn.addEventListener('click',()=>window.location.reload());
+  pauseBtn.addEventListener('click',controlPause);
+  field.addEventListener('click',removeItem);
+});
+
+function controlPause(){
   if(!isPaused){
     timerObj.pause();
-    stage.removeEventListener('click',removeItem);
+    field.removeEventListener('click',removeItem);
     pauseBtn.innerHTML=`<i class="fas fa-play"></i>`;
     bgSound.pause();
     playAudio(alertSound);
   }
   else{
     timerObj.resume();
-    stage.addEventListener('click',removeItem);
+    field.addEventListener('click',removeItem);
     pauseBtn.innerHTML=`<i class="fas fa-pause"></i>`;
     playAudio(bgSound);
   }
   isPaused=!isPaused;
-});
-stage.addEventListener('click',removeItem);
-
+}
 function removeItem(event){
   const {target}=event;
   if(!target.classList.contains("item")) return;
@@ -46,8 +51,7 @@ function removeItem(event){
   counter.innerText-=1;
   if(counter.innerText==0){
     resultMessage.innerText=winMessage;
-    endGame();
-    playAudio(winSound);
+    endGame(true);
   }
 }
 function playAudio(audio){
@@ -56,13 +60,15 @@ function playAudio(audio){
 }
 
 function startGame(){
+  introWindow.style.opacity=0;
+  introWindow.style.zIndex=-1;
   playAudio(bgSound);
-  result.style.zIndex="-1";
+  result.style.zIndex=-1;
   result.style.opacity=0;
   counter.innerText=10;
   pauseBtn.innerHTML=`<i class="fas fa-pause"></i>`;
   timerObj.start();
-  showRandomItems();
+  showRandomItems(counter.innerText);
 
   const items=document.querySelectorAll(".item");
   items.forEach(item=>{
@@ -73,18 +79,23 @@ function startGame(){
   }
   );
 }
-function endGame(){
-  stage.removeEventListener('click',removeItem);
+function endGame(isWinning){
+  field.removeEventListener('click',removeItem);
+  pauseBtn.removeEventListener('click',controlPause);
+  playBtn.removeEventListener('click',startGame);
+
   bgSound.pause();
   timerObj.pause();
-  result.style.zIndex="1";
+  result.style.zIndex=1;
   result.style.opacity=1;
+  if(isWinning) playAudio(winSound);
+  else playAudio(alertSound);
 }
-function showRandomItems(){
-  const {width,height}=stage.getBoundingClientRect();
+function showRandomItems(num){
+  const {width,height}=field.getBoundingClientRect();
   const fragment = new DocumentFragment();
 
-  for(let i=0;i<counter.innerText;i++){
+  for(let i=0;i<num;i++){
     const random=(max)=>Math.floor(Math.random() * max);
     const top=random(height);
     const left=random(width);
@@ -95,7 +106,7 @@ function showRandomItems(){
     item.style.transform=`translate(${left}px,${top}px)`;
     fragment.appendChild(item);
   }
-  for(let i=0;i<counter.innerText;i++){
+  for(let i=0;i<num;i++){
     const random=(max)=>Math.floor(Math.random() * max);
     const top=random(height);
     const left=random(width);
@@ -106,7 +117,7 @@ function showRandomItems(){
     item.style.transform=`translate(${left}px,${top}px)`;
     fragment.appendChild(item);
   }
-  stage.appendChild(fragment);
+  field.appendChild(fragment);
 }
 class Timer{
   constructor(duration,display,printDone){
@@ -131,7 +142,7 @@ class Timer{
     if(this.duration<=0){
       clearInterval(this.interval);
       this.interval=null;
-      this.callBack("Done");
+      this.callBack(false);
     }
     this.mins=`${parseInt(this.duration/60)}`;
     this.secs=`${this.duration%60}`;
