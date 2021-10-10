6a0c6c3df62332047f3f2598f521a6341e3ac5d7
HongBeen <redbean-2@naver.com>
2021-10-11 01:28:25 +0900

    game.js의 end메서드를 stop으로 바꾸고, pause버튼 핸들링메서드와 통합

--- CarrotGame/src/main.js
+++ CarrotGame/src/main.js
@@ -8,7 +8,7 @@ const BUG_COUNT = 10;
 const DURATION_SEC = 5;
 
 const WINNER = "✨ WOW! YOU WON! ✨";
-const LOSER = "👽 YOU LOST! TRY AGAIN! 👽";
+const LOSER = "👽 YOU LOSE!! 👽";
 const REPLAY = "🐞 REPLAY? 🐞";
 
 const gameReplayBanner = new PopUp();
@@ -24,19 +24,21 @@ gameIntroBanner.setClickListner(game.start);
 game.setGameReadyListner(gameIntroBanner.show);
 game.setGameResumeListner(gameReplayBanner.hide);
 game.setGameStopListner((reason) => {
+	let message;
 	switch (reason) {
 		case Reason.win:
-			gameReplayBanner.showWithText(WINNER);
+			message=WINNER;
 			break;
 		case Reason.lose:
-			gameReplayBanner.showWithText(LOSER);
+			message=LOSER;
 			break;
 		case Reason.pause:
-			gameReplayBanner.showWithText(REPLAY);
+			message=REPLAY;
 			break;
 		default:
 			throw new Error("not valid error");
 	}
+	gameReplayBanner.showWithText(message);
 });
 gameReplayBanner.setClickListner(game.init);
 game.init();
